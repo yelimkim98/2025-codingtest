1bd467afa0523960c09f8b4becb033c8d3b72a4f
yerimkim <kiel0103@naver.com>
2025-04-24 19:30:52 +0900

    42861

--- programmers/java/42861_섬_연결하기.java
+++ programmers/java/42861_섬_연결하기.java
@@ -1,7 +1,57 @@
 import java.util.*;
 
+class Solution {
+    public int solution(int n, int[][] costs) {
+        List<Edge> edges = new ArrayList<>();
+        for (int[] cost : costs) {
+            edges.add(new Edge(cost[0], cost[1], cost[2]));
+        }
+        Collections.sort(edges);
+        
+        /*
+        arr 초기화 : 아무 간선도 포함시키지 않음 -> 모든 노드의 root가 자기 자신
+        
+        비용이 낮은 순서대로 간선을 선택하여 모든 간선 탐색
+        - 간선의 양 끝 노드가  같은 root 를 갖고있으면  cycle이 형성되어있는 것임 -> 미포함
+        - 간선의 양 끝 노드가 아직 같은 root 를 갖지 않으면 해당 간선을 포함해도 cycle이 형성되지 않음 -> 포함
+        */
+        int[] arr = new int[n];
+        
+        for (int i=0; i<n; i++) {
+            arr[i] = i;
+        }
+        
+        int totalCost = 0;
+        
+        for (Edge e : edges) {
+            int n1Root = find(e.n1, arr);
+            int n2Root = find(e.n2, arr);
+            
+            // System.out.printf("노드 %d root %d, 노드 %d root %d (비용 %d)\n", e.n1, n1Root, e.n2, n2Root, e.cost);
+            
+            if (n1Root != n2Root) {
+                totalCost += e.cost;
+                arr[n1Root] = n2Root;
+            }
+            // for (int i=0; i<n; i++) {
+            //     System.out.print(arr[i] + " ");
+            // }
+            // System.out.println();
+        }
+        
+        return totalCost;
+    }
+    // arr 을 이용해서 node 의 root를 찾는다.
+    public int find(int node, int[] arr) {
+        if (arr[node] == node) {  // 본인이 root임
+            return node;
+        }
+        return find(arr[node], arr);
+    }
+}
 class Edge implements Comparable<Edge> {
-    int n1, n2;
+    int n1;
+    int n2;
     int cost;
     public Edge(int n1, int n2, int cost) {
         this.n1 = n1;
@@ -13,65 +63,3 @@ class Edge implements Comparable<Edge> {
         return Integer.compare(this.cost, e.cost);
     }
 }
-
-class Solution {
-    public int solution(int n, int[][] costs) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        for (int i=0; i<costs.length; i++) {
-            pq.offer(new Edge(costs[i][0], costs[i][1], costs[i][2]));
-        }
-        int[] nodeRoots = new int[n];
-        
-        for (int i=0; i<n; i++) { nodeRoots[i] = i; }
-        
-        int cost = 0;
-        
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
-            
-            // 이미 연결되어있는 경우 (사이클 형성하는 case)
-            if (nodeRoots[e.n1] == nodeRoots[e.n2]) {
-                continue;
-            }
-            cost += e.cost;
-            // update nodeRoots
-            updateRoots(e, nodeRoots);
-            boolean finished = true;
-            for (int i=0; i<n; i++) {
-                if (nodeRoots[i] != nodeRoots[e.n1]) {
-                    finished = false;
-                }
-            }
-            if (finished) break;
-        }
-        return cost;
-    }
-    
-    void updateRoots(Edge newEdge, int[] roots) {
-        /* originalRoot1 != originalRoot2 */
-        int originalRoot1 = roots[newEdge.n1];
-        int originalRoot2 = roots[newEdge.n2];
-        
-        /*
-         originalRoot1 또는 originalRoot2를 root 노드로 갖는 모든 노드들의 root 정보를
-         originalRoot1, originalRoot2, newEdge.n1, newEdge.n2 중 최솟값으로 update
-         */
-        int min = Math.min(originalRoot1, originalRoot2);
-        min = Math.min(min, newEdge.n1);
-        min = Math.min(min, newEdge.n2);
-        
-        for (int i=0; i<roots.length; i++) {
-            if (roots[i] == originalRoot1 || roots[i] == originalRoot2) {
-                roots[i] = min;
-            }
-        }
-    }
-}
-/*
-     *4
- 0--------1
-          |
-          | *2
-          |
-          3
- */
